65f5532748ce8b05b428418083ab1086cf4db86d
Krishan Patel <krishan711@gmail.com>
2026-01-20 21:49:43 +0000

    .

--- src/useWeb3Transaction.ts
+++ src/useWeb3Transaction.ts
@@ -38,13 +38,10 @@ export const useWeb3Transaction = (): [Web3TransactionDetails, (newTransactionPr
     if (!transactionPromise) {
       return;
     }
-    console.log('waitForTransactionPromise', transactionPromise);
     try {
       const newTransaction = await transactionPromise;
-      console.log('newTransaction', newTransaction)
       setTransaction(newTransaction);
     } catch (newError: unknown) {
-      console.log('waitForTransactionPromise error', newError);
       setError(newError as Error);
     }
     setTransactionPromise(null);
@@ -58,13 +55,10 @@ export const useWeb3Transaction = (): [Web3TransactionDetails, (newTransactionPr
     if (!transaction) {
       return;
     }
-    console.log('waitForTransaction', transaction);
     try {
       const newReceipt = await transaction.wait();
-      console.log('newReceipt', newReceipt)
       setReceipt(newReceipt);
     } catch (newError: unknown) {
-      console.log('waitForTransaction error', newError);
       setError(newError as Error);
       setReceipt(null);
     }
