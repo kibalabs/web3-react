0ce73f4ac1f902967a52973817436809f970481a
Krishan Patel <krishan711@gmail.com>
2026-01-27 17:45:56 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * from './model';
 export * from './Web3AccountContext';
 export * from './useWeb3Contract';
 export * from './useWeb3Transaction';
-export { web3Initialize } from './Web3AccountContext';
